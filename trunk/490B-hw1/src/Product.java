e24b5995ca6fbf8782b13d72b76b47058266d38e
julieimpola@gmail.com <julieimpola@gmail.com@4616c4c8-dd7d-7c6f-8f57-2b6d3ce684d3>
2013-10-22 22:26:47 +0000

    Added VendingMachine, Product, and Ingredient classes.

---
+++ trunk/490B-hw1/src/Product.java
@@ -0,0 +1,50 @@
+
+public class Product {
+	private String name;
+	private double price;
+	private int timesSold;
+	private Ingredient[] ingredients;
+	
+	public Product() {
+		this("", 0.0, 0, null);
+	}
+	
+	public Product(String name, double price, int timesSold, Ingredient[] ingredients) {
+		this.name = name; 
+		this.price = price;
+		this.timesSold = timesSold;
+		this.ingredients = ingredients;
+	}
+	
+	public String name() {
+		return name;
+	}
+	
+	public double price() {
+		return price;
+	}
+	
+	public int timesSold() {
+		return timesSold;
+	}
+	
+	public Ingredient[] ingredients() {
+		return ingredients;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public void setTimesSold(int timesSold) {
+		this.timesSold = timesSold;
+	}
+	
+	public void setIngredients(Ingredient[] ingredients) {
+		this.ingredients = ingredients;
+	}
+}
