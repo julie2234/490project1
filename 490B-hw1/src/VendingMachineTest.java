0f0145a0110dbb4dd01e9dcc39e43cbda8afddcd
fiddip <fiddip@uw.edu>
2013-10-25 05:33:10 +0000

    Created the skeleton for testing VendingMachine

---
+++ 490B-hw1/src/VendingMachineTest.java
@@ -0,0 +1,111 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author PaulaFiddi
+ *
+ */
+public class VendingMachineTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#VendingMachine()}.
+	 */
+	@Test
+	public void testVendingMachine() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#mainMenu()}.
+	 */
+	@Test
+	public void testMainMenu() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#addProduct()}.
+	 */
+	@Test
+	public void testAddProduct() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#deleteProduct()}.
+	 */
+	@Test
+	public void testDeleteProduct() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#buyProduct()}.
+	 */
+	@Test
+	public void testBuyProduct() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#increaseIngredientsInventory()}.
+	 */
+	@Test
+	public void testIncreaseIngredientsInventory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#decreaseIngredientsInventory(Product, double)}.
+	 */
+	@Test
+	public void testDecreaseIngredientsInventory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#checkIngredientsInventory()}.
+	 */
+	@Test
+	public void testCheckIngredientsInventory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link VendingMachine#statistics()}.
+	 */
+	@Test
+	public void testStatistics() {
+		fail("Not yet implemented");
+	}
+
+}
